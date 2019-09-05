aa69a22b7612231cb434c8e1b1e4e66c1808b148
Richard Drexel <richdrexel@gmail.com>
2019-09-05 15:36:12 -0400

    Added BubbleSort class, created BubbleSortTest method in main program.

---
+++ Algorithms/Algorithms/Program.cs
@@ -0,0 +1,27 @@
+﻿using Algorithms.SortingAlgorithms;
+using System;
+using System.Linq;
+
+namespace Algorithms
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            BubbleSortTest();
+        }
+
+        private static void BubbleSortTest()
+        {
+            int[] arr = { 90, 1, 68, 34, 100, 67, 13, 12, 11, 89, 1000, 20, 77, 43, 9 };
+
+            IBubbleSort bubbleSort = new BubbleSort();
+
+            int[] sorted = bubbleSort.Sort(arr);
+
+            Console.WriteLine("Bubble Sort:");
+
+            sorted.ToList().ForEach(n => Console.WriteLine(n));
+        }
+    }
+}
---
+++ Algorithms/Algorithms/SortingAlgorithms/BubbleSort.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Algorithms.SortingAlgorithms
+{
+    internal interface IBubbleSort
+    {
+        int[] Sort(int[] arr);
+    }
+
+    internal class BubbleSort : IBubbleSort
+    {
+        public int[] Sort(int[] arr)
+        {
+            return SortArray(arr);
+        }
+
+        private int[] SortArray(int[] arr)
+        {
+            int temp;
+
+            for (int j = 0; j <= arr.Length - 2; j++)
+            {
+                for (int i = 0; i <= arr.Length - 2; i++)
+                {
+                    if (arr[i] > arr[i + 1])
+                    {
+                        temp = arr[i + 1];
+                        arr[i + 1] = arr[i];
+                        arr[i] = temp;
+                    }
+                }
+            }
+
+            return arr;
+        }
+    }
+}
